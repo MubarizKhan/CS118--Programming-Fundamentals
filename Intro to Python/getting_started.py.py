df90f6e6ef20c148fe44adc52d6a1ad541a225f1
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-07-20 00:43:29 +0500

    Add files via upload

---
+++ Intro to Python/getting_started.py.py
@@ -0,0 +1,22 @@
+import math
+from math import sqrt
+
+x = math.sqrt(4)
+print (x)
+
+print (math.sqrt(69))
+
+y = "awddf"
+print(type(y))
+
+from math import pi
+
+z = pi * pi * 1
+print("value of z is", z)
+
+s = """ This is a multiple
+        line string, my friendsssssssssss """
+
+print (s)
+
+
