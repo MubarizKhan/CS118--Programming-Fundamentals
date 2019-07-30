d918b0dcc4de90d8230f8669e5306d9abda3d711
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-07-30 22:56:33 +0500

    Add files via upload

---
+++ assignment3/a01.py
@@ -0,0 +1,13 @@
+def code_holder(num1, num2):
+    
+    #### YOUR CODE GOES HERE ####
+    #### make sure you save the return value in the variable: v
+   	v = 0	
+	v = num1 + num2
+
+	return v
+    #### DO NOT WRITE AFTER THIS LINE
+
+	
+
+
